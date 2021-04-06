60d66685381a7871e61cf446e9e109abe8109248
Aashar Mehmood <aasharmehmood3@gmail.com>
2021-04-06 05:29:05 -0700

    init

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const emailExpression = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+const emailExpression = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 //select buttons for actions
 const SUBMIT_BTN = document.getElementById('submit');
